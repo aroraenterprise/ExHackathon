1390218a234b89f3c386ac09b8eef07ee0556814
aroraenterprise <saj.arora24@gmail.com>
2016-11-13 12:56:39 -0500

    adding goal stuff

--- Map_2/app/scripts/climate.js
+++ Map_2/app/scripts/climate.js
@@ -13,18 +13,61 @@ $('#carto-climate-explorer').on('shown.bs.modal', function () {
 function cartoSql(sql, callback) {
   $.getJSON('https://sajarora.carto.com/api/v2/sql?q=' + sql + '&api_key=4ff8b9ddf97c920f633367e7b9ce3f9dd8aa9f42', callback);
 }
-var dataset = {};
+var data = [];
+var rcp = '26';
+function DoD3() {
+  console.log(data);
+  var chart = new CanvasJS.Chart("chart-container",
+    {
+      zoomEnabled: true,
+      title: {
+        text: "Temperature vs. RCP Level: " + rcp,
+      },
+      animationEnabled: true,
+      axisX: {
+        labelAngle: 30
+      },
+
+      axisY: {
+        includeZero: false
+      },
+
+      data: data  // random generator below
+
+    });
+  chart.render();
+}
+
+function RenderChart(input) {
+  var limit = 100;    //increase number of dataPoints by increasing this
+
+  var y = 0;
+  var dataSeries = { type: "line" };
+  var dataPoints = [];
+
+  $.each(input, function (key, val) {
+    dataPoints.push({
+      x: new Date(val.date),
+      y: val.tasmin
+    });
+  });
+
+  dataSeries.dataPoints = dataPoints;
+  data.push(dataSeries);
+  DoD3();
+}
 
 $('#carto-explorer-btn').on('click', function () {
   //do the request
   cartoSql('SELECT count(*) FROM data_1', function (data) {
     $("#raw-dataset").prepend("<p>Found: " + data.rows[0].count + ' records...</p>');
   });
-  cartoSql('SELECT * FROM data_1 LIMIT 100', function (data) {
+  cartoSql("SELECT * FROM data_1 WHERE scenario='rcp"+rcp+"'", function (data) {
+    RenderChart(data.rows);
+
     $("#loader").remove();
     var html = '<pre>'
     $.each(data.rows, function (key, val) {
-      console.log(val);
       html += '\n' + val.date + ' - ' + val.tasmin + '(C)';
     });
     html += '</pre>'
@@ -45,4 +88,4 @@ $("#carto-maps-btn").on('click', function () {
   $("#carto-dashboard").css({ display: 'block' });
 })
 
-// $('#carto-explorer-btn').trigger('click');
+$('#carto-explorer-btn').trigger('click');
