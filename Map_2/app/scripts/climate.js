d0c23ba0126f3c7b303adb629bf934b9675a0256
aroraenterprise <saj.arora24@gmail.com>
2016-11-13 13:20:12 -0500

    new stuff

--- Map_2/app/scripts/climate.js
+++ Map_2/app/scripts/climate.js
@@ -14,14 +14,14 @@ function cartoSql(sql, callback) {
   $.getJSON('https://sajarora.carto.com/api/v2/sql?q=' + sql + '&api_key=4ff8b9ddf97c920f633367e7b9ce3f9dd8aa9f42', callback);
 }
 var data = [];
-var rcp = '26';
+var rcp = '85';
 function DoD3() {
   console.log(data);
   var chart = new CanvasJS.Chart("chart-container",
     {
       zoomEnabled: true,
       title: {
-        text: "Temperature vs. RCP Level: " + rcp,
+        text: "Orlando Temperature vs. RCP Level: " + rcp,
       },
       animationEnabled: true,
       axisX: {
@@ -57,15 +57,15 @@ function RenderChart(input) {
   DoD3();
 }
 
-$('#carto-explorer-btn').on('click', function () {
+function GetCartoData(rcp) {
   //do the request
+   $("#loader").css({display:'block'});
   cartoSql('SELECT count(*) FROM data_1', function (data) {
     $("#raw-dataset").prepend("<p>Found: " + data.rows[0].count + ' records...</p>');
   });
   cartoSql("SELECT * FROM data_1 WHERE scenario='rcp"+rcp+"'", function (data) {
     RenderChart(data.rows);
-
-    $("#loader").remove();
+   $("#loader").css({display:'none'});
     var html = '<pre>'
     $.each(data.rows, function (key, val) {
       html += '\n' + val.date + ' - ' + val.tasmin + '(C)';
@@ -73,19 +73,21 @@ $('#carto-explorer-btn').on('click', function () {
     html += '</pre>'
     $("#raw-dataset").append(html);
   });
+}
+
+$('#carto-explorer-btn').on('click', function () {
+  GetCartoData(rcp);
 })
 
 $("#carto-graphs-btn").on('click', function () {
   $("#raw-dataset").css({ display: 'none' });
   $(this).css({ display: 'none' });
-  $("#carto-graphs").css({ display: 'block' });
-  $("#carto-maps-btn").css({ display: 'inline-block' })
-})
-
-$("#carto-maps-btn").on('click', function () {
-  $(this).css({ display: 'none' });
-  $("#carto-graphs").css({ display: 'none' });
   $("#carto-dashboard").css({ display: 'block' });
 })
 
-$('#carto-explorer-btn').trigger('click');
+// $('#carto-explorer-btn').trigger('click');
+
+$(".rcp-click").on('click', function () {
+  rcp = ($(this).data('rcp'));
+  GetCartoData(rcp);
+})
